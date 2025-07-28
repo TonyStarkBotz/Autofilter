3fd781045d1e9f9a725ae21c9e55e74de6b3a8b4
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2025-07-28 14:51:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -200,7 +200,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """📂 <i><a href="https://t.me/Avengers_Alliance">{file_name}</a></i>\n\n"""
+    CAPTION = """📂 <i><a href="https://t.me/TheAvengersLeague">{file_name}</a></i>\n\n"""
 
     IMDB_TEMPLATE_TXT = """<b>ʜᴇʏ {message.from_user.mention}, ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ ǫᴜᴇʀʏ {search}.
 
